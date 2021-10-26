35da1b6d8ff748cbcca19577cf1f4ed0b0bb5501
rluna15 <58538880+rluna15@users.noreply.github.com>
2021-10-26 15:06:41 -0400

    initial commit

---
+++ dist/script.js
@@ -0,0 +1,2 @@
+"use strict";var testString="This is a test!";console.log(testString);var fruits=["mango","papaya","lychee"];fruits.map((function(t){console.log(t)}));
+//# sourceMappingURL=script.js.map
